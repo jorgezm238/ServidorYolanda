e1d7bd23bd9f3a23e0bb6e83e1dfdc9b9755885f
Jorge Zardain Monforte <wrn47806@educastur.es>
2024-11-14 12:48:48 +0100

    comit

--- pruebaYolanda/ej1,1.php
+++ pruebaYolanda/ej1,1.php
@@ -7,6 +7,27 @@
 <body>
     <form action="ej1.php" method="POST">
         <table>
+        <?php 
+        $filas= 3;
+        $columnas=2;
+
+        for ($i=0; $i <$filas ; $i++) { 
+            echo "<tr>";
+            for ($j=0; $j <$columnas ; $j++) { 
+                $indice = ($i * $columnas) + $j + 1;
+                echo"<td>E.$i.$j</td>";
+                echo"<td><input type 'text' name ='va   lor$indice'/></td>";
+
+            }
+                echo"</tr>";
+        }
+        
+        
+        ?>    
+        
+
+    <!-- 
+ 
             <tr>
                 <td>E.0.0</td>
                 <td><input type="text" name="valor1" /></td>
@@ -24,7 +45,7 @@
                 <td><input type="text" name="valor5" /></td>
                 <td>E.2.1</td>
                 <td><input type="text" name="valor6" /></td>
-            </tr>
+            </tr>-->
         </table>
         <br>
         <input type="submit" name="calcular" value="Calcular">
